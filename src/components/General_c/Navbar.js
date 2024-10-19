af658685bc42bf5c8ec7c5014f205e9585c21603
kevin-magu <wanjaukevinmagu@gmail.com>
2024-10-19 13:02:17 +0300

    works

--- src/components/General_c/Navbar.js
+++ src/components/General_c/Navbar.js
@@ -1,8 +1,30 @@
 import React, {useEffect} from 'react'
 import ActionButton from './ActionButton'
+import { FaEnvelope } from 'react-icons/fa';
+import { FaWhatsapp } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa';
+
 
 
 function Navbar() {
+  useEffect(() =>{
+    //select get in touch link after component is mounted
+  const getInTouch = document.querySelector('.get-in-touch-link')
+  const contacts = document.querySelector('.contacts')
+
+  //define click function
+  const handleClick = () =>{
+    contacts.style.display = "flex"
+  };
+  getInTouch.addEventListener('click', handleClick)
+
+  //clean up function to remove the event listener when the component unmounts
+  return()=>{
+    getInTouch?.removeEventListener('click', handleClick);
+  }
+
+  }, []);
+
   return (
     <div>
         <nav className='navigation-wrapper'>
@@ -11,7 +33,15 @@ function Navbar() {
               <li>Home</li>
               <li>Projects</li>
               <li>Innovation</li>
-              <li className='get-in-touch-link'>Get In Touch</li>
+
+              <span className='get-in-touch-link'>
+                <span className='span-text'>Get In Touch </span>
+                <div className='contacts'>
+                  <p> <FaEnvelope /> Email: info.kevin.co.ke</p>
+                  <p> <FaPhone /> Call : 0797692537</p>
+                </div>
+              </span>
+
             </ul>
         </nav>
     </div>
