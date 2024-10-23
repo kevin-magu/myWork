c86f5e981ab449a6f004b6be4e4d79f1e32d296a
kevin-magu <wanjaukevinmagu@gmail.com>
2024-10-23 23:21:16 +0300

    work

--- src/components/General_c/Navbar.js
+++ src/components/General_c/Navbar.js
@@ -3,6 +3,11 @@ import ActionButton from './ActionButton'
 import {Link,useNaviage} from "react-router-dom"
 import { FaChevronDown } from 'react-icons/fa'
 
+// innovation images 
+import aiCardimage from '../../images/ai-card-image.jpg' 
+import spaceCardImage from '../../images/space-card-image2.jpg'
+import climateChangeImage from '../../images/other-card-image.jpg'
+
 //icons from fa imorts
 import { FaEnvelope } from 'react-icons/fa';
 import { FaWhatsapp } from 'react-icons/fa';
@@ -12,6 +17,7 @@ import { FaHouse } from 'react-icons/fa6';
 import { FaTools } from 'react-icons/fa';
 import { FaBrain } from 'react-icons/fa';
 import { FaBookOpen } from 'react-icons/fa6';
+import { FaSearch } from 'react-icons/fa';
 
 
 
@@ -19,30 +25,49 @@ import { FaBookOpen } from 'react-icons/fa6';
 function Navbar() {
   useEffect(() =>{
     //select get in touch link after component is mounted
-   const getInTouch = document.querySelector('.get-in-touch-link')
-   const contacts = document.querySelector('.contacts')
+   const getInTouch = document.querySelector('.get-in-touch-link');
+   const contacts = document.querySelector('.contacts');
+   const searchResults = document.querySelector('.search-results');
+   const innovationLink = document.querySelector('.innovation-link');
+   const projectsLink = document.querySelector('.projects-link');
+
 
   //define click function
   const handleClick = () =>{
     contacts.style.display = "flex"
   };
+
   getInTouch.addEventListener('click', handleClick)
 
    //close contacts when the document is clicked
    const handleContactClosing = (event) => {
     if(!contacts.contains(event.target) && (!getInTouch.contains(event.target))){
       contacts.style.display='none';
-    }
+  }//else if(!searchResults.contains(event.target)&& (!searchResults.contains(event.target))){
+    //   searchResults.style.display='none'
+    // }
   };
   document.addEventListener('click', handleContactClosing)
+
+  //projects , innovation clicking
+  function handleProjectInnovationClick(){
+    searchResults.style.display='block'
+  }
+  innovationLink.addEventListener('click', handleProjectInnovationClick);
+  projectsLink.addEventListener('click', handleProjectInnovationClick);
 
   //clean up function to remove the event listener when the component unmounts
   return()=>{
     getInTouch?.removeEventListener('click', handleClick);
-    document?.removeEventListener('click', handleContactClosing)
+    document?.removeEventListener('click', handleContactClosing);
+    innovationLink?.removeEventListener('click', handleProjectInnovationClick);
+    projectsLink?.removeEventListener('click', handleProjectInnovationClick);
   };
 
   }, []);
+
+  
+
  
 
   return (
@@ -59,8 +84,8 @@ function Navbar() {
             
             <ul>
               <Link className='link' to='/'> <FaHouse className='navbar-icon'/> Home</Link>
-              <Link className='link'> <FaTools className='navbar-icon'/> Projects <FaChevronDown className='nav-dropdown'/> </Link>
-              <Link className='link'> <FaBrain className='navbar-icon'/> Innovation <FaChevronDown className='nav-dropdown'/> </Link>
+              <Link className='link'> <FaTools className='navbar-icon projects-link'/> Projects <FaChevronDown className='nav-dropdown'/> </Link>
+              <Link className='link'> <FaBrain className='navbar-icon innovation-link'/> Innovation <FaChevronDown className='nav-dropdown'/> </Link>
               <Link className='link'> <FaBookOpen className='navbar-icon'/> Thoughts</Link>
 
               <span className='get-in-touch-link'>
@@ -71,6 +96,12 @@ function Navbar() {
                   <p> <FaLinkedin className='nav-icon'/> Linkedin</p>
                 </div>
               </span>
+              {/* <Link className='link navbar-icon'> <FaSearch /> </Link> */}
+              <div className='search-results-wrapper'></div>
+              <div className='search-results'>
+                <button className='result-close-button'>Close</button>
+
+              </div>
             </ul>
         
         </nav>
